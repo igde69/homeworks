0f74e65bff98de23b02697e75dc7adfc8308ccce
demakhinigor <tau3ceti@gmail.com>
2023-03-08 18:24:16 +0300

    first easy solution homework 1.1

--- lvl_1/task_1.1.py
+++ lvl_1/task_1.1.py
@@ -6,3 +6,10 @@ my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start M
 
 # Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
 # Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+
+# first easy solution
+# my_favorite_songs.split(",") 
+print (my_favorite_songs[:14])
+print (my_favorite_songs[-13:])
+print (my_favorite_songs[16:30])
+print (my_favorite_songs[-26:-15])
