d0fa4bfbe15c70bf7f42cd89f926b406cf84d005
demakhinigor <tau3ceti@gmail.com>
2023-03-19 20:52:54 +0300

    Strange solution task 2.2

--- lvl_2/task_2.2.py
+++ lvl_2/task_2.2.py
@@ -9,18 +9,37 @@
 def quarter_of(month):
     pass
 
+#       ***
+
 import datetime
+import locale
 number = input("Введите номер месяца: ")
 month = int(number)
-if int(number) < 1 or int(number) > 12:print("Такого месяца нет!")
+
+if int(number) < 1 or int(number) > 12:
+    print("Такого месяца нет!")
+    exit()
 else:
     date = datetime.datetime.strptime(number, "%m")
-    # Код форматирования функции .strptime() - месяц в виде десятичного числа (%m)
+# Код форматирования функции .strptime() - месяц в виде десятичного числа (%m)
+    locale.setlocale(category = locale.LC_ALL,locale = "Russian")
+# Здесь локализуем язык 
     month_name = date.strftime("%B")
-    # Код форматирования функции .strptime() - месяц как полное название (en_US) (%B)
-    print("Название месяца:",month_name)
+# Код форматирования функции .strptime() - полное название месяца (%B)
+# Интересно до названия месяца можно добраться непосредственно без даты ?
 
-#quarter ={}
-#if month in quarter:
-#    print('Является частью', [])
+def quaters(months):
+    if months in [1,2,3]:
+        return 'первого'
+    elif months in [4,5,6]:
+        return 'второго'
+    elif months in [7,8,9]:
+        return 'третьего'
+    else:
+        return 'четвёртого'
 
+print('Месяц', month, '(',month_name,')', 'является частью',quaters(month), 'квартала')     
+
+# Вывод
+# Введите номер месяца: 7
+# Месяц 7 ( Июль ) является частью третьего квартала
