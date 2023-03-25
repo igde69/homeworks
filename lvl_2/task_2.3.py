a2b8c334a270917ef48ec85e2712fae1705fea3f
demakhinigor <tau3ceti@gmail.com>
2023-03-25 21:12:43 +0300

    Solution of exercise 2.3

--- lvl_2/task_2.3.py
+++ lvl_2/task_2.3.py
@@ -7,5 +7,42 @@
 # switch_it_up(10000) -> None
 # Использовать условный оператор if-elif-else нельзя!
 
-def switch_it_up(number):
-    pass
+                ###
+
+# определяем функцию, которая принимает на входе целое число
+#def switch_it_up(num): 
+
+# составляем список слов для цифр от 0 до 9
+#    words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+
+# выделяем из входного числа его цифру сопоставляем цифру со словом из списка слов
+#    digits = [int(n) for n in str(num)]
+#    words_list = [words[n] for n in digits]
+
+# с помощью метода .join() объединяем слово c пробелом и возвращаем результат
+#    return ' '.join(words_list)
+
+#num = int(input('Введите число: '))
+#words = switch_it_up(num)
+#print(words)
+# ???             
+
+                ###
+
+# определяем функцию, которая принимает на входе целое число
+def switch_it_up(digit):
+    if digit > 9:
+        return "None"
+# составляем список слов для цифр от 0 до 9    
+    words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+    return words[digit]
+
+digit = int(input('Введите число: '))
+words = switch_it_up(digit)
+print(words)
+
+
+#Введите число: 7
+#Seven
+#Введите число: 100
+#None
